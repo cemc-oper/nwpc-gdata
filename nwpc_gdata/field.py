ea06a1a4a98b8208d7d291d1789255be5b78d366
perillaroc <perillaroc@gmail.com>
2020-04-03 14:45:17 +0800

    :sparkles: add load_field_bytes.

--- nwpc_gdata/field.py
+++ nwpc_gdata/field.py
@@ -3,15 +3,37 @@ import typing
 
 import pandas as pd
 
+from .index_retrieval import IndexRetrieval
+from .grib import load_bytes_from_index
 
-def load_field(
+
+def load_field_bytes(
         system: str,
         stream: str,
         data_type: str,
         data_name: str,
         start_time: datetime.datetime or pd.Timestamp,
         forecast_time: pd.Timedelta,
+        parameter: str,
+        level_type: str,
+        level: int,
+        index_retrieval: IndexRetrieval,
         data_class: str = "od",
-):
-
-    return
+) -> bytes or None:
+    grib_index = index_retrieval.query(
+        system=system,
+        stream=stream,
+        data_type=data_type,
+        data_name=data_name,
+        start_time=start_time,
+        forecast_time=forecast_time,
+        parameter=parameter,
+        level_type=level_type,
+        level=level,
+        data_class=data_class,
+    )
+    if grib_index is None:
+        return None
+    else:
+        raw_bytes = load_bytes_from_index(grib_index)
+        return raw_bytes
